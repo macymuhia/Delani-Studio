9f104b8f844b2c707a8b8119971191dddb1f74ac
macymuhia <mercy8muhia@gmail.com>
2019-06-09 16:26:49 +0300

    Add jquery function to make preloader show

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //Preloader
+    $(window).on("load", function() {
+        preloaderFadeOutTime = 3000;
+
+        function hidePreloader() {
+            var preloader = $('.preloader');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }
+        hidePreloader();
+    });
+});
